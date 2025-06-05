1d2fb1c8a379a157a623e94da9700733f7efe6ab
MariaPoutiou <marypoutiou@gmai.com>
2025-06-05 23:44:06 +0300

    Activate filter button in catalog and style it

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const burgerBtn = document.querySelector(".header__burger-btn");
   const headerSection = document.querySelector(".header-section");
   const body = document.querySelector("body");
+  const filterMobileBtnArea = document.querySelector('.catalog-tools__filter');
+  const filterHeaderBtnArea = document.querySelector(' .catalog-filter__header-btn');
+    const asideCatalog = document.querySelector('.catalog-filter');
 
   if (burgerBtn && headerSection) {
     burgerBtn.addEventListener("click", () => {
       headerSection.classList.toggle("is-shown");
+      body.classList.toggle("not-scrolled");
+    });
+  }
+  if (filterMobileBtnArea && asideCatalog) {
+    filterMobileBtnArea.addEventListener("click", () => {
+      filterMobileBtnArea.classList.toggle("is-chosen");
+      asideCatalog.classList.toggle("is-shown");
+      body.classList.toggle("not-scrolled");
+    });
+  }
+    if (filterHeaderBtnArea && asideCatalog) {
+    filterHeaderBtnArea.addEventListener("click", () => {
+      filterHeaderBtnArea.classList.toggle("is-chosen");
+      asideCatalog.classList.toggle("is-shown");
       body.classList.toggle("not-scrolled");
     });
   }
